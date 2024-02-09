af6fc740cc7c8407914e2e3387774d62455386d6
Jhay <macalinaojhay9@gmail.com>
2024-02-09 17:39:57 +0800

    PieChart data from db

--- component/charts/DoughnutAndPie.js
+++ component/charts/DoughnutAndPie.js
@@ -4,45 +4,45 @@ import {
 } from "react-native-chart-kit";
 import { Dimensions, View, ActivityIndicator } from 'react-native';
 
-const data = [
-    {
-        name: "Seoul",
-        population: 21500000,
-        color: "rgba(131, 167, 234, 1)",
-        legendFontColor: "#7F7F7F",
-        legendFontSize: 15
-    },
-    {
-        name: "Toronto",
-        population: 2800000,
-        color: "#F00",
-        legendFontColor: "#7F7F7F",
-        legendFontSize: 15
-    },
-    {
-        name: "Beijing",
-        population: 527612,
-        color: "red",
-        legendFontColor: "#7F7F7F",
-        legendFontSize: 15
-    },
-    {
-        name: "New York",
-        population: 8538000,
-        color: "#ffffff",
-        legendFontColor: "#7F7F7F",
-        legendFontSize: 15
-    },
-    {
-        name: "Moscow",
-        population: 11920000,
-        color: "rgb(0, 0, 255)",
-        legendFontColor: "#7F7F7F",
-        legendFontSize: 15
-    }
-];
+// const data = [
+//     {
+//         name: "Seoul",
+//         population: 21500000,
+//         color: "rgba(131, 167, 234, 1)",
+//         legendFontColor: "#7F7F7F",
+//         legendFontSize: 15
+//     },
+//     {
+//         name: "Toronto",
+//         population: 2800000,
+//         color: "#F00",
+//         legendFontColor: "#7F7F7F",
+//         legendFontSize: 15
+//     },
+//     {
+//         name: "Beijing",
+//         population: 527612,
+//         color: "red",
+//         legendFontColor: "#7F7F7F",
+//         legendFontSize: 15
+//     },
+//     {
+//         name: "New York",
+//         population: 8538000,
+//         color: "#ffffff",
+//         legendFontColor: "#7F7F7F",
+//         legendFontSize: 15
+//     },
+//     {
+//         name: "Moscow",
+//         population: 11920000,
+//         color: "rgb(0, 0, 255)",
+//         legendFontColor: "#7F7F7F",
+//         legendFontSize: 15
+//     }
+// ];
 
-export const DoughnutAndPie = ({ }) => {
+export const DoughnutAndPie = ({ data }) => {
     const screenWidth = Dimensions.get('window').width;
 
     const chartConfig = {
@@ -52,19 +52,25 @@ export const DoughnutAndPie = ({ }) => {
         }
     };
 
+    console.log("Chart: ", data);
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <PieChart
-                style={{
-                    marginVertical: 8,
-                    ...chartConfig.style
-                }}
-                data={data}
-                width={screenWidth*.94}
-                height={220}
-                chartConfig={chartConfig}
-                accessor={"population"}
-            />
-        </View>
+        <>
+            {data
+                &&
+                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <PieChart
+                        style={{
+                            marginVertical: 8,
+                            ...chartConfig.style
+                        }}
+                        data={data}
+                        width={screenWidth * .94}
+                        height={220}
+                        chartConfig={chartConfig}
+                        accessor={"totalInputs"}
+                    />
+                </View>}
+        </>
     )
 }
